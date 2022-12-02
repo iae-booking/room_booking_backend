69b4b4ab3d47c5fc437ad9da75dc9b5b5cebf7e5
Benson <s980245@bles.tp.edu.tw>
2022-12-03 03:32:35 +0800

    update

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from router import hello, auth, hotels
 from db import models
 from db.database import engine
 from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["x-apigateway-header", "Content-Type", "X-Amz-Date"],
+)
+
 
 app.include_router(hello.router)
 app.include_router(auth.router)
