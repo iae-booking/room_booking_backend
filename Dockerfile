9827363d3199443d0fc565194e4ddf881bcfd99b
bensonhi <55286621+bensonhi@users.noreply.github.com>
2022-12-04 22:58:56 +0800

    CI/CD for backend (#1)
    * added CI/CD workflow
    
    * test
    
    * update dependency
    
    * comment pytest
    
    * update yaml
    
    * update
    
    * fix tabbing
    
    * update dependency
    
    * update dependency
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: Benson <s980245@bles.tp.edu.tw>

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM public.ecr.aws/lambda/python:3.7
+COPY . .
+RUN ls
+RUN yum install postgresql-devel* -y
+RUN yum install gcc -y
+RUN pip install -r ./requirements.txt
+CMD ["main.handler"]
--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from fastapi import Depends, FastAPI, HTTPException
 from router import hello, auth, hotels
 from db import models
 from db.database import engine
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(root_path="/test/")
+
+origins = [
+    "https://main.d2nlm8wd08s9mv.amplifyapp.com",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(hello.router)
 app.include_router(auth.router)
@@ -14,3 +28,5 @@ app.include_router(hotels.router)
 @app.get('/')
 def home():
     return "This is the entry point"
+
+handler = Mangum(app=app)
