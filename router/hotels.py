42826914e366bb7287203ceaa0503575c2a6d3b3
KiritoWang5204 <73264444+KiritoWang5204@users.noreply.github.com>
2022-12-11 20:37:11 +0800

    Add add_room, order, Search_hotels

--- router/hotels.py
+++ router/hotels.py
@@ -38,6 +38,24 @@ def create_hotel(hotel_info: schemas.Hotel, db: Session = Depends(get_db), membe
     crud.create_hotel(db,hotel_info, member_id)
     return {'status': 'success'}
 
+@router.post("/create", response_model=schemas.Room)
+def add_room(room_info: schemas.Room, db: Session = Depends(get_db), member_id: int = Depends(get_member_id), hotel_id: int = 1):
+    if member_id is False:
+        raise HTTPException(status_code=404, detail="User not found")
+    rooms = crud.add_room(db,room_info, hotel_id)
+    return rooms
+
+@router.post("/order", response_model=schemas.Order)
+def order(order_info: schemas.Order, db: Session = Depends(get_db), member_id: int = Depends(get_member_id), hotel_id: int = 1, room_id: int = 1):
+    if member_id is False:
+        raise HTTPException(status_code=404, detail="User not found")
+    orders = crud.order(db,order_info, hotel_id, room_id, member_id)
+    return orders
+
+@router.get("/search", response_model=List[schemas.Hotel])
+def Search_hotels(db: Session = Depends(get_db)):
+    rooms = crud.create_hotel(db)
+    return rooms
 
 @router.post("/rates", response_model=schemas.RequestResult)
 def rate_hotel(rate_info: schemas.Rate, db: Session = Depends(get_db), member_id: int = Depends(get_member_id)):
